ac218b4358e98db1e2aa727a5af57f761d26e9b1
Afrida Rahman <afridarahman365@gmail.com>
2018-11-20 23:27:00 +0000

    finally done

--- js/script.js
+++ js/script.js
@@ -8,21 +8,61 @@
 var userChoice = "";
 var computerChoice = "";
 var winner = "";
-
+var computerScore = 0;
+var point = 1;
+var myPoint = 0;
+function updateTeams(){
+    $("#myPoints").text(myPoint);
+    $("#computerPoints").text(computerScore);
+}
 
 $("button").click(function() {
     var randomNumber= Math.random();
    $("#userChoice").text(userChoice);
    userChoice = $("#input").val ();
     if (randomNumber <1 && randomNumber> 0.66) {
-        $("#computerChoice").html("rock");
-    }
+        $("#computerChoice").html(computerChoice);
+        computerChoice = "rock";
+}
     else if ( randomNumber <0.66 && randomNumber >0.33) {
-        $("#computerChoice").html("paper");
+        $("#computerChoice").html(computerChoice);
+        computerChoice = "paper";
+}
+    else {
+        $("#computerChoice").html(computerChoice);
+        computerChoice="scissors";
+}
+    if (userChoice === "rock" && computerChoice === "scissors") {
+        $("#winner").html("User Wins!");
+        myPoint += point;
+    }
+    else if (userChoice === "rock" && computerChoice === "paper") {
+        $("#winner").html("Computer Wins!");
+        computerScore += point;
+    }
+    else if (userChoice === "paper" && computerChoice === "rock") {
+        $("#winner").html("User Wins!");
+        myPoint += point;
+    }
+    else if (userChoice === "paper" && computerChoice === "scissors") {
+        $("#winner").html("Computer Wins!");
+        computerScore += point;
+    }
+    else if (userChoice === "scissors" && computerChoice === "rock") {
+        $("#winner").html("Computer Wins!");
+        computerScore += point;
+    }
+    else if (userChoice === "scissors" && computerChoice === "paper") {
+        $("#winner").html("User Wins!");
+        myPoint += point;
+    }
+    else if (userChoice === computerChoice) {
+        $("#winner").html("It's a draw! Duel again.");
     }
     else {
-        $("#computerChoice").html("scissors");
+        $("#winner").html("Choose a real weapon!");
     }
+    updateTeams()
 });
 // DOCUMENT READY FUNCTION BELOW
 
